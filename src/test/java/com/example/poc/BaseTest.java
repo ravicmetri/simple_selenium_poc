e2cf8a6fc87fe22709d4867afa8095ba54ee94e8
ravirajm63 <ravirajmetri85@gmail.com>
2025-12-05 00:46:12 +0530

    done changes in base test to for handling webdriver manager22222

--- src/test/java/com/example/poc/BaseTest.java
+++ src/test/java/com/example/poc/BaseTest.java
@@ -6,18 +6,28 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
 	protected WebDriver driver;
 
 	@BeforeMethod
 	public void setUp() {
-		ChromeOptions options = new ChromeOptions();
-		// add your options...
-		driver = new ChromeDriver(options);
+		/*
+		 * ChromeOptions options = new ChromeOptions(); // add your options... driver =
+		 * new ChromeDriver(options);
+		 * 
+		 * // options.addArguments("--headless=new"); //
+		 * options.addArguments("--no-sandbox"); //
+		 * options.addArguments("--disable-dev-shm-usage"); driver = new
+		 * ChromeDriver(options);
+		 */
 
-		// options.addArguments("--headless=new");
-		// options.addArguments("--no-sandbox");
-		// options.addArguments("--disable-dev-shm-usage");
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		if ("true".equalsIgnoreCase(System.getenv("CI"))) {
+			options.addArguments("--headless=new");
+		}
 		driver = new ChromeDriver(options);
 	}
 
