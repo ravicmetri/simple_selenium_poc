5090c4b5e04997e8fcc6b9c301ef02d4444904d3
ravirajm63 <ravirajmetri85@gmail.com>
2025-12-07 20:42:53 +0530

    allure results

--- src/test/java/com/example/poc/SimpleTest.java
+++ src/test/java/com/example/poc/SimpleTest.java
@@ -2,11 +2,18 @@ package com.example.poc;
 
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Allure;
+
 public class SimpleTest extends BaseTest {
 
 	@Test
 	public void openDummyUrl() throws InterruptedException {
+
 		driver.get("https://practicetestautomation.com/practice-test-login/");
+		Allure.step("Open page");
+		// test actions...
+		// Allure.addAttachment("screenshot", "image/png", inputStreamOfScreenshot,
+		// "png");
 		String title = driver.getTitle();
 		Thread.sleep(5000);
 		// assertTrue(title.toLowerCase().contains("example"));
